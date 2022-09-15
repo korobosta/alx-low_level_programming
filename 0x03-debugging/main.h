0988f2a1a9822c01f878473299888ea7c01babf2
Kevin Ongulu <kevinkorobosta@gmail.com>
2022-09-15 14:16:52 -0700

    updates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,19 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Kevin Ongulu
+ * Desc: Header file containing declarations for all functions
+ *       used in the debugging directory.
+ */
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-#endif /* HOLBERTON_H */
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif 
+/* MAIN_H */
