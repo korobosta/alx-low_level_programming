42a35554c86cdc1dad6b0179522313b1fbfda344
Kevin Ongulu <kevinkorobosta@gmail.com>
2022-09-15 12:02:26 -0700

    2-largest_number.c  2-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ * Auth: Kevin Ongulu
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x03-debugging directory.
+ */
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
