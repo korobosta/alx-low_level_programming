007df53eab9f370c615af01da624e7bc92347768
Kevin Ongulu <kevinkorobosta@gmail.com>
2022-09-15 13:23:58 -0700

    2-largest_number.c, main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Kevin Ongulu
- * Desc: Header file containing declarations for all functions
- *       used in the 0x03-debugging directory.
- */
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
