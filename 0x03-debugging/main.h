5580bfb6d20a3a8d939c11c550f85530199b1107
Kevin Ongulu <kevinkorobosta@gmail.com>
2022-09-15 13:48:31 -0700

    updates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
