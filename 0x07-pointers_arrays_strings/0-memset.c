80c37ebc94a60f2201177e7b87d22e33659367e5
Kevin Ongulu <kevinkorobosta@gmail.com>
2022-09-26 12:15:08 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,7 +17,6 @@
 void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
-	
 	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
@@ -25,3 +24,4 @@ void *_memset(void *s, int c, size_t n)
 
 	return (memory);
 }
+
