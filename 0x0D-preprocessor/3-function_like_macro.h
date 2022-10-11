b3a9ad42f0275464e20c79a0a3c32eb1036a9016
Kevin Ongulu <kevinkorobosta@gmail.com>
2022-10-11 12:59:28 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,13 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Kevin Ongulu
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
-
